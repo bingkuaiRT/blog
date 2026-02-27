99239b4159403cb1b7b63f557cf207dc74d6ca61
bingkuaiRT <1428069050@qq.com>
2026-02-27 21:31:31 +0800

    Site updated: 2026-02-27 21:31:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/26/雪国2022》这不是徒劳还能是什么？/","2026/02/27/steam新品节demo浅谈/"];function toRandomPost(){
+var posts=["2026/02/27/steam新品节demo浅谈/","2026/02/26/雪国2022》这不是徒劳还能是什么？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
