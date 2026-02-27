ffd9a5bd7c4eb6d048acdd9d8d4d70d132587e41
bingkuaiRT <1428069050@qq.com>
2026-02-27 12:25:51 +0800

    Site updated: 2026-02-27 12:25:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/25/hello-world/","2026/02/26/xuguo/"];function toRandomPost(){
+var posts=["2026/02/26/xuguo/","2026/02/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
